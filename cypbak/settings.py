e4365eed601a5e610f699b02bd125b5da03ced29
victor15-design <chiemico678@gmail.com>
2026-08-09 22:25:54 +0100

    few changes

--- cypbak/settings.py
+++ cypbak/settings.py
@@ -112,6 +112,7 @@ DATABASES = {
     'default': dj_database_url.config(
         default=config('DATABASE_URL'),
         conn_max_age=600,
+        conn_health_checks=True,
         ssl_require=True
     )
 }
